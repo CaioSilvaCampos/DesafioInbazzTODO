80b0ec8abc5ce1fc0bf660f8c77ab2cb5bcd4489
CaioSilvaCampos <caioiosilva19@gmail.com>
2025-08-05 23:23:50 -0300

    Adicionei a documentação do swagger nos endpoints da API

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,32 +1,64 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
-@Controller('categorias')
+@ApiTags('Categories')
+@Controller('categories')
 export class CategoriasController {
   constructor(private readonly categoriasService: CategoriasService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar uma nova categoria'
+  })
+  @ApiBody({
+    type: CreateCategoriaDto
+  })
   create(@Body() createCategoriaDto: CreateCategoriaDto) {
     return this.categoriasService.create(createCategoriaDto);
   }
-
+  
   @Get()
+  @ApiOperation({
+    summary: 'Listar todas as categorias'
+  })
   findAll() {
     return this.categoriasService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Listar uma categoria especifíca'
+  })
+  @ApiParam({
+    name: 'id', description: 'id da categoria', example: 1
+  })
   findOne(@Param('id') id: string) {
     return this.categoriasService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Put(':id')
+  @ApiOperation({
+    summary: 'Atualizar informações de uma categoria'
+  })
+  @ApiParam({
+    name: 'id', description: 'id da categoria', example: 1
+  })
+  @ApiBody({
+    type: UpdateCategoriaDto
+  })
   update(@Param('id') id: string, @Body() updateCategoriaDto: UpdateCategoriaDto) {
     return this.categoriasService.update(+id, updateCategoriaDto);
   }
 
+  @ApiOperation({
+    summary: 'Excluir uma categoria'
+  })
+  @ApiParam({
+    name: 'id', description: 'id da categoria', example: 1
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.categoriasService.remove(+id);
--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,32 +1,64 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Todos')
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
+  @ApiOperation({
+    summary: "Criar uma nova tarefa"
+  })
+  @ApiBody({
+    type: CreateTodoDto
+  })
   @Post()
   create(@Body() createTodoDto: CreateTodoDto) {
     return this.todosService.create(createTodoDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Listar todas as tarefas'
+  })
   findAll() {
     return this.todosService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Listar detalhes de uma tarefa específica'
+  })
+  @ApiParam({
+    name: 'id', description: 'id da tarefa', example: '1'
+  })
   findOne(@Param('id') id: string) {
     return this.todosService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Put(':id')
+  @ApiOperation({
+    summary:'Atualizar os detalhes de uma tarefa'
+  })
+  @ApiParam({
+    name: 'id', description: 'id da tarefa', example: 1
+  })
+  @ApiBody({
+    type: UpdateTodoDto
+  })
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todosService.update(+id, updateTodoDto);
   }
 
+  @ApiOperation({
+    summary: 'Excluir uma tarefa'
+  })
+  @ApiParam({
+    name: 'id', description: 'id da tarefa', example: 1
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.todosService.remove(+id);
